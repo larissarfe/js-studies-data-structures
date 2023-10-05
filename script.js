9fff5bf9d0b39550f4648af3cbd87f59359c6fec
Larissa <larissarf.dev@gmail.com>
2023-10-05 08:41:24 -0300

    coding-challenge3

--- script.js
+++ script.js
@@ -56,6 +56,47 @@ const restaurant = {
   },
 };
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+
+const gameSet = [...new Set(gameEvents.values())];
+
+console.log(gameSet);
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+gameEvents.delete(64);
+
+console.log(gameEvents);
+
+//3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+//4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽️ GOAL
+
+for (const [key, value] of gameEvents)
+  key <= 45
+    ? console.log(`[FIRST HALF]: ${value}`)
+    : console.log(`[SECOND HALF]: ${value}`);
+
+/*
 // ============================================================> MAPS - ITERATION:
 
 // We can populate a new map by:
@@ -269,7 +310,7 @@ for (const order of ordersSet) {
 
 const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
-// to know only which different positionsthere are in our restaurant - AN UNIQUE ARRAY OF STAFF
+// to know only which different positions there are in our restaurant - AN UNIQUE ARRAY OF STAFF
 
 const staffUnique = [...new Set(staff)]; // CREATE AN ARRAY AROUND THIS AND UNPACK IT  // WE PASS THE ITERABLE IN
 
@@ -413,7 +454,8 @@ for (const [i, el] of menu.entries()) {
 
 //BUT WE CAN DESTRUCTURE IT -> const [i, el]
 
-console.log(menu.entries()); */
+console.log(menu.entries()); 
+
 
 // -------------------------------------- CODING CHALLENGE
 const game = {
